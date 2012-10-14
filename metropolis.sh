b72fa6b207c15a38f8b3bd81accab6d3d5bd4262
Gwylim Ashley <gwylim.a@gmail.com>
2012-10-14 17:08:47 +0200

    different parameters for metropolis

--- metropolis.sh
+++ metropolis.sh
@@ -1,4 +1,4 @@
-for ((l=20; l<=25; l+=5))
+for l in 10 20 30 50
 do
-    nice -n19 pypy metropolis.py dist m$l $l $[l*l*500000] $[l*l*100] 1.42
+    nice -n19 pypy metropolis.py dist m$l $l $[l*l*500000] $[l*l*100] 1.424
 done
